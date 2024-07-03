3bd7b56858adea1aac96dbdb3baa2e933a26a5a8
Vighnesh-b <viggie198@gmail.com>
2024-07-03 22:48:19 +0530

    made changes to profile

--- app/src/main/java/com/example/pawpal10/MainActivity.java
+++ app/src/main/java/com/example/pawpal10/MainActivity.java
@@ -2,16 +2,15 @@ package com.example.pawpal10;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -20,54 +19,44 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    FirebaseUser user;
-    BottomNavigationView bottomNavigationView;
-
-    // Map to store menu item IDs and their corresponding fragments
-    Map<Integer, Fragment> fragmentMap = new HashMap<>();
-    ListView listView;
-    String[] name = {"Home", "Category", "Paw", "Medical", "Profile"};
-    ArrayAdapter<String> arrayAdapter;
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private BottomNavigationView bottomNavigationView;
+    private Map<Integer, Fragment> fragmentMap;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private SearchView searchView;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize Firebase Auth
+        initializeFirebaseAuth();
+        initializeBottomNavigationView();
+        initializeFragments();
+        checkUserAuthentication();
+
+        initializeSearchView();
+
+    }
+
+    private void initializeFirebaseAuth() {
         auth = FirebaseAuth.getInstance();
+    }
 
+    private void initializeBottomNavigationView() {
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        // Populate fragmentMap with menu item IDs and their corresponding fragments
-        fragmentMap.put(R.id.home_icon, new HomeFragment());
-        fragmentMap.put(R.id.catagory_icon, new catagoryFragment());
-        fragmentMap.put(R.id.paw_icon, new pawFragment());
-        fragmentMap.put(R.id.medical_icon, new checkupFragment());
-        fragmentMap.put(R.id.profile_icon, new profileFragment());
-
-        // Set initial fragment
-        replaceFragment(new HomeFragment());
-
-        // Check if user is logged in
-        user = auth.getCurrentUser();
-        if (user == null) {
-            // Redirect to login if not logged in
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
-        }
-
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // Get the fragment corresponding to the selected menu item ID from fragmentMap
                 Fragment selectedFragment = fragmentMap.get(item.getItemId());
                 if (selectedFragment != null) {
                     replaceFragment(selectedFragment);
@@ -78,22 +67,93 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initializeFragments() {
+        fragmentMap = new HashMap<>();
+        fragmentMap.put(R.id.home_icon, new HomeFragment());
+        fragmentMap.put(R.id.catagory_icon, new catagoryFragment());
+        fragmentMap.put(R.id.paw_icon, new pawFragment());
+        fragmentMap.put(R.id.medical_icon, new checkupFragment());
+        fragmentMap.put(R.id.profile_icon, new profileFragment());
+
+        // Set initial fragment
+        replaceFragment(new HomeFragment());
+    }
+
+    private void checkUserAuthentication() {
+        user = auth.getCurrentUser();
+        if (user == null) {
+            redirectToLogin();
+        }
+    }
+
+    private void redirectToLogin() {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initializeSearchView() {
+        searchView = findViewById(R.id.searchView);
+        listView = findViewById(R.id.searchListView);
+        listView.setVisibility(View.GONE);
+
+        arrayList = new ArrayList<>();
+        arrayList.add("Home");
+        arrayList.add("Category");
+        arrayList.add("Paw");
+        arrayList.add("Medical");
+        arrayList.add("Profile");
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        // Set query hint programmatically
+        searchView.setQueryHint("Search");
+
+        // Handle clicks on SearchView to expand and focus
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the SearchView with one tap
+                if (!searchView.isIconified()) {
+                    return;
+                }
+                searchView.setIconified(false);
+                searchView.requestFocus();
+            }
+        });
+
+        // Handle query text change for filtering ListView
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                listView.setVisibility(View.VISIBLE);
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        // Handle closing SearchView to hide ListView
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                listView.setVisibility(View.GONE);
+                return false;
+            }
+        });
+    }
+
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, fragment); // Assuming your FrameLayout ID is frameLayout
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
     }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
